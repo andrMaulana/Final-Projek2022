40d7358841944d9a0907a5e50713146af4df02af
andrMaulana <andryymaulana2000@gmail.com>
2022-06-04 00:17:46 +0700

    make it better

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\AsmaulController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/asmaulHusna',[AsmaulController::class, 'store']);
+Route::get('/asmaulHusna',[AsmaulController::class, 'index']);
+Route::get('/asmaulHusna/{id}',[AsmaulController::class, 'detailAsmaul']);
+Route::get('/asmaulHusna/search',[AsmaulController::class, 'search']);
