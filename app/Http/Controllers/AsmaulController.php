96fed20f10f10d8e56fa70bacda9d0e9a38f4d17
andrMaulana <andryymaulana2000@gmail.com>
2022-06-06 13:39:28 +0700

    menambahkan beberapa dokumentasi

--- app/Http/Controllers/AsmaulController.php
+++ app/Http/Controllers/AsmaulController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 use App\Models\AsmaulHusna;
 use Illuminate\Http\Request;
@@ -60,18 +59,58 @@ class AsmaulController extends Controller
         return response()->json($response, Response::HTTP_OK);
     }
 
-    public function search(Request $request){
-        $search = $request->cari;
 
-        // mengambil data dari table pegawai sesuai pencarian data
-		$asmaul = DB::table('asmaul_husnas')
-		->where('latin','like',"%".$search."%")
-		->paginate();
-        $response = [
-            'message' => 'Asmaul Husna yang di cari',
-            'data' => $asmaul
-        ];
-        return response()->json($response, Response::HTTP_OK);
+    // Proses Update Data Asmaul Husna
+    public function update(Request $request, $id)
+    {
+        // Mencari id data asmaul husna seperti data asmaul husna mana yang akan di rubah datanya
+        $transaction = AsmaulHusna::findOrFail($id);
+        // jika data sudah di temukan tahap selanjutnya validasi terlebih dahulu agar tidak sembarangan memasukan data
+        $validate = Validator::make($request->all(),[
+            'latin' => ['required'],
+            'arabic' => ['required'],
+            'terjemahan' => ['required'],
+            'keterangan' => ['required'],
+            'amalan' => ['required']
+        ]);
+
+        // tahap ini akan memunculkan error apabila data tidak sesuai dengan validasi di atas
+        if ($validate->fails()) {
+            return response()->json($validate->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+
+        // setelah data berhasil di validasi maka tahap selanjutnya proses mengupdate data yang akan di masukan ke database
+        try {
+            $transaction->update($request->all());
+            $response = [
+                'message' => 'Data Asmaul Husna Berhasi Diubah',
+                'data' => $transaction
+            ];
+            return response()->json($response, Response::HTTP_OK);
+        } catch (QueryException $e) {
+            return response()->json([
+                'message' => 'Data Asmaul Husna gagal disimpan',
+                'data' => "Gagal" . $e->errorInfo
+            ]);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $transaction = AsmaulHusna::findOrFail($id);
+        try {
+            $transaction->delete();
+            $response = [
+                'message' => 'Data Asmaul Husna Berhasil Dihapus',
+            ];
+            return response()->json($response, Response::HTTP_OK);
+        } catch (QueryException $e) {
+            return response()->json([
+                'message' => 'Data Asmaul Husna gagal dihapus',
+                'data' => "Gagal" . $e->errorInfo
+            ]);
+        }
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\AsmaulController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
@@ -7,17 +6,26 @@ use Illuminate\Http\Request;
 // route untuk proses login
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
+
 // route data user akan di tampilkan apabila sudah login
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
 // route untuk proses logout
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+// Route Untuk menampilkan data Asmaul Husna
+Route::get('/asmaulHusna',[AsmaulController::class, 'index']);
+Route::get('/asmaulHusna/{id}',[AsmaulController::class, 'detailAsmaul']);
+
 
 //
 Route::middleware(['auth:api'])->group(function(){
 Route::post('/asmaulHusna',[AsmaulController::class, 'store']);
-Route::get('/asmaulHusna',[AsmaulController::class, 'index']);
-Route::get('/asmaulHusna/{id}',[AsmaulController::class, 'detailAsmaul']);
+Route::put('/asmaulHusna/{id}',[AsmaulController::class, 'update']);
+Route::delete('/AsmaulHusna/{id}',[AsmaulController::class, 'destroy']);
 });
 
